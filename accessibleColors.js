df1ed17ae9fcb513a057b2f71521cdfc72e3458d
Kevin Gutowski <kgutowski@berkeley.edu>
2017-06-17 13:17:10 -0700

    Figuring out how to update the figure

--- accessibleColors.js
+++ accessibleColors.js
@@ -1,7 +1,7 @@
 function gridData() {
   var data = new Array();
-  var xpos = 1;
-  var ypos = 1;
+  var xpos = 0;
+  var ypos = 0;
   var columnPos = 0;
   var rowPos = 0;
   var width = 1;
@@ -9,11 +9,11 @@ function gridData() {
 
   //iterate over rows
 
-  for (var row = 0; row <= 255; row++) {
+  for (var row = 0; row <= 100; row++) {
     data.push( new Array() );
 
     // interate for cells
-    for (var column = 0; column <= 255; column++) {
+    for (var column = 0; column <= 360; column++) {
       data[row].push({
         x: xpos,
         y: ypos,
@@ -29,7 +29,7 @@ function gridData() {
       columnPos += 1;
     }
     // reset x position after row is complete
-    xpos = 1;
+    xpos = 0;
     // reset column position after row is complete
     columnPos = 0;
     // increment the y position for the new row
@@ -43,22 +43,44 @@ function gridData() {
 
 
 window.onload = function() {
-console.log("hello");
-var svg = d3.select('svg');
+  var svg = d3.select('svg');
 
-var row = svg.selectAll('.row')
-    .data(gridData())
-  .enter().append('g')
-    .attr('class', "row");
+  var row = svg.selectAll('.row')
+      .data(gridData())
+    .enter().append('g')
+      .attr('class', "row");
 
   var column = row.selectAll('.square')
     .data(function(d) { return d;})
   .enter().append('rect')
     .attr('x', function(d) { return d.x;})
     .attr('y', function(d) { return d.y;})
-    .attr('width', function(d) { return d.width;})
-    .attr('height', function(d) { return d.height;})
+    .attr('width', 0)
+    .attr('height', 0)
     .attr('fill', function(d) {
-      var tempColor = "rgb(" + d.columnPos + "," + d.rowPos + ",255)"
+      var tempColor = "rgb(" + d.columnPos + "," + d.rowPos + ",255)";
       return tempColor;});
+
+  column
+    .attr('width', function(d) { return d.width;})
+    .attr('height', function(d) { return d.height;});
+
+    var colorCounter = 0;
+    d3.select("button").on('click', function() {
+      colorCounter++;
+      if (colorCounter%2 == 0) {
+        column.attr('fill', function(d) {
+        var tempColor = "rgb(" + d.columnPos + "," + d.rowPos + ",255)";
+        return tempColor;});
+      } else {
+        column.attr('fill', function(d) {
+        var tempColor = "rgb(255," + d.columnPos + "," + d.rowPos + ")";
+        return tempColor;});
+      }
+
+    });
+
 }
+
+
+
