fadeeb9f8cba82d01ef4c7edd0806c6059022019
Kevin Gutowski <kgutowski@berkeley.edu>
2017-07-20 01:17:36 -0700

    added color contrast curve in canvas… but its blurry so SVG it is

--- accessibleColors.js
+++ accessibleColors.js
@@ -22,38 +22,83 @@ var textColor = {
   b: 100,
 }
 
+var currentColor = initialColor;
+var currentTextColor = white;
+var accessibilityValue = 4.5;
+
 function accessibleColors() {
-  var canvas = document.querySelector('#colorSpace'),
-    width = canvas.width,
-    height = canvas.height,
-    context = canvas.getContext('2d'),
-    image = context.createImageData(width,height);
+  var colorSpaceCanvas = document.querySelector('#colorSpace'),
+      cSWidth = colorSpaceCanvas.width,
+      cSHeight = colorSpaceCanvas.height,
+      cSContext = colorSpaceCanvas.getContext('2d'),
+      cSImage = cSContext.createImageData(cSWidth,cSHeight);
 
   function gridImageData(hue) {
     //iterate over rows
-    for (var row = 0, i=-1; row < width; ++row) {
+    for (var row = 0, i=-1; row < cSHeight; ++row) {
       // interate for cells
-      for (var column = 0; column < height; ++column) {
+      for (var column = 0; column < cSWidth; ++column) {
         var tempColor = {
           h: hue,
           s: column,
-          v: height - row
+          v: cSHeight - row
         }
         var tempRGBColor = HSVtoRGB(normHSV(tempColor));
-        image.data[++i] = Math.round(tempRGBColor.r*255);
-        image.data[++i] = Math.round(tempRGBColor.g*255);
-        image.data[++i] = Math.round(tempRGBColor.b*255);
-        image.data[++i] = 255;
+        cSImage.data[++i] = Math.round(tempRGBColor.r*255);
+        cSImage.data[++i] = Math.round(tempRGBColor.g*255);
+        cSImage.data[++i] = Math.round(tempRGBColor.b*255);
+        cSImage.data[++i] = 255;
       }
     }
-    console.log(image);
+    cSContext.putImageData(cSImage,0,0);
 
-    context.putImageData(image,0,0);
+    var accessibilityPath = "M";
+    for (var column = 0; column <= 100; column++) {
+      for (var row = 0; row <= 100; row++) {
+        console.log(checkColorContrast(hue, column, row));
+        if (checkColorContrast(hue, column, row)) {
+          var scaledRow = 100 - row;
+          accessibilityPath = accessibilityPath + " " + column + " " + scaledRow;
+          break;
+        }
+      }
+    }
+    p = new Path2D(accessibilityPath);
+    cSContext.stroke(p);
   }
 
-  gridImageData(20);
+  var hueChannelCanvas = document.querySelector('#hueChannel'),
+      hCWidth = hueChannelCanvas.width,
+      hCHeight = hueChannelCanvas.height,
+      hCContext = hueChannelCanvas.getContext('2d'),
+      hCImage = hCContext.createImageData(hCWidth, hCHeight);
 
+  function hueChannelData() {
+    for (var column = 0, i=-1; column < 360; ++column) {
+      var tempColor = {
+        h: column,
+        s: 100,
+        v: 100
+      }
+      var tempRGBColor = HSVtoRGB(normHSV(tempColor));
+      hCImage.data[++i] = Math.round(tempRGBColor.r*255);
+      hCImage.data[++i] = Math.round(tempRGBColor.g*255);
+      hCImage.data[++i] = Math.round(tempRGBColor.b*255);
+      hCImage.data[++i] = 255;
+    }
+    hCContext.putImageData(hCImage,0,0);
+  }
+
+  gridImageData(currentColor.h);
+  hueChannelData();
 }
 
-
-
+function checkColorContrast(hue, x, y) {
+  var tempColor = {
+    h: hue,
+    s: x,
+    v: y
+  }
+  var contrast = getColorContrastHSV(tempColor, currentTextColor);
+  return (contrast <= accessibilityValue)
+}
