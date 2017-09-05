bc206e285e5a6493bd04ac6410d1f4c8c4395df2
Kevin Gutowski <kgutowski@berkeley.edu>
2017-09-05 02:09:22 -0700

    Fix second accessibility line display colors

--- accessibleColors.js
+++ accessibleColors.js
@@ -823,9 +823,9 @@ function accessibleColors() {
       }
 
       if (isBrightnessLarger1) {
-        var indexStart2 = saturation2X;
-      } else {
         var indexStart2 = brightness2X;
+      } else {
+        var indexStart2 = saturation2X;
       }
 
       var renderClosestAccessibileColors2 = d3.select('#renderClosestAccessibileColors2').selectAll('.closestsColors2').data(closestColorsArray2);
