9dffb35cd4657ee6d0c331886b48a90f89b933e9
Kevin Gutowski <kskiviolin@gmail.com>
2022-01-13 22:46:36 -0800

    add star birth

--- pkmjp/sets/s9/script.js
+++ pkmjp/sets/s9/script.js
@@ -2,6 +2,210 @@ let sites = ['fullcomp', 'cardrush','bigweb','yuyu-tei','clabo'];
 let quantityCheckboxState = true
 let priceCheckboxState = true
 
+function parseSummaryTable(tableData) {
+    tableData.pop() // remove last empty object
+    var table = $('<table></table>');
+    var tableHead = $(`
+      <colgroup>
+        <col>
+        <col span=4 class='column-group'>
+        <col>
+        <col span=4 class='column-group'>
+        <col>
+        <col span=4 class='column-group'>
+        <col>
+        <col span=4 class='column-group'>
+        <col>
+        <col span=4 class='column-group'>
+      </colgroup>
+      <tr>
+          <th></th>
+          <th colspan='4' class='column-header-group'>Fullcomp</th>
+          <th class="spacer" />
+          <th colspan='4' class='column-header-group'>Cardrush</th>
+          <th class="spacer" />
+          <th colspan='4' class='column-header-group'>Bigweb</th>
+          <th class="spacer" />
+          <th colspan='4' class='column-header-group'>Yuyu-tei</th>
+          <th class="spacer" />
+          <th colspan='4' class='column-header-group'>C-labo</th>
+      </tr>
+      <tr>
+        <th>Rarity</th>
+        <!-- Fullcomp -->
+        <th class='quantityItem quantityValue'>Stock</th>
+        <th class='quantityItem'>Diff</th>
+        <th class='priceItem priceValue'>Price</th>
+        <th class='priceItem'>Diff</th>
+        <th class="spacer" />
+        <!-- Cardrush -->
+        <th class='quantityItem quantityValue'>Stock</th>
+        <th class='quantityItem'>Diff</th>
+        <th class='priceItem priceValue'>Price</th>
+        <th class='priceItem'>Diff</th>
+        <th class="spacer" />
+        <!-- Bigweb -->
+        <th class='quantityItem quantityValue'>Stock</th>
+        <th class='quantityItem'>Diff</th>
+        <th class='priceItem priceValue'>Price</th>
+        <th class='priceItem'>Diff</th>
+        <th class="spacer" />
+        <!-- Yuyu-tei -->
+        <th class='quantityItem quantityValue'>Stock</th>
+        <th class='quantityItem'>Diff</th>
+        <th class='priceItem priceValue'>Price</th>
+        <th class='priceItem'>Diff</th>
+        <th class="spacer" />
+        <!-- C-labo -->
+        <th class='quantityItem quantityValue'>Stock</th>
+        <th class='quantityItem'>Diff</th>
+        <th class='priceItem priceValue'>Price</th>
+        <th class='priceItem'>Diff</th>
+      </tr>
+    `)
+    table.append(tableHead)
+    $(tableData).each(function (i, rowData) {
+      let rowString = `
+          <tr class="table-content">
+          <td>${rowData.card_rarity}</td>
+      `
+      sites.forEach((site,index)=>{
+        let quantityDiffValue = rowData[`${site}_stock_diff`]
+        let priceDiffValue = rowData[`${site}_price_diff`]
+        let columnGroupString = `
+          <td class='quantityItem quantityValue'>${rowData[`${site}_stock_trend`]}${numberWithCommas(rowData[`${site}_latest_stock`])}</td>
+          <td class='quantityItem ${getColorClass(quantityDiffValue)}'>${formatDiff(quantityDiffValue)}</td>
+          <td class='priceItem priceValue'>${rowData[`${site}_price_trend`]}${numberWithCommas(rowData[`${site}_latest_price`])}</td>
+          <td class='priceItem ${getColorClass(priceDiffValue)}'>${formatDiff(priceDiffValue)}</td>
+        `
+        if (sites.length - 1 != index) {
+          columnGroupString = columnGroupString + '<td class="spacer" />'
+        }
+
+        rowString = rowString + columnGroupString
+      })
+      rowString = rowString + '</tr>'
+      table.append(rowString);
+    });
+    return table;
+}
+
+function parseMainTable(data) {
+  var table = $('<table></table>');
+  var tableHead = $(`
+    <colgroup>
+      <col class='col-group-number'>
+      <col class='col-group-name'>
+      <col class='col-group-spacer'>
+      <col span=4 class='column-group column-group-fullcomp'>
+      <col class='col-group-spacer'>
+      <col span=4 class='column-group column-group-cardrush'>
+      <col class='col-group-spacer'>
+      <col span=4 class='column-group column-group-bigweb'>
+      <col class='col-group-spacer'>
+      <col span=4 class='column-group column-group-yuyu'>
+      <col class='col-group-spacer'>
+      <col span=4 class='column-group column-group-clabo'>
+    </colgroup>
+    <tr>
+        <th></th>
+        <th></th>
+        <th class="spacer" />
+        <th colspan='4' class='column-header-group'>Fullcomp</th>
+        <th class="spacer" />
+        <th colspan='4' class='column-header-group'>Cardrush</th>
+        <th class="spacer" />
+        <th colspan='4' class='column-header-group'>Bigweb</th>
+        <th class="spacer" />
+        <th colspan='4' class='column-header-group'>Yuyu-tei</th>
+        <th class="spacer" />
+        <th colspan='4' class='column-header-group'>C-labo</th>
+    </tr>
+    <tr class='second-header'>
+        <td>#</td>
+        <td class='header-name'>Name</td>
+        <td class='spacer'></td>
+        <td class="quantityItem quantityValue">Stock</td>
+        <td class="quantityItem">Diff</td>
+        <td class="priceItem priceValue">Price</td>
+        <td class="priceItem">Diff</td>
+        <td class='spacer'></td>
+        <td class="quantityItem quantityValue">Stock</td>
+        <td class="quantityItem">Diff</td>
+        <td class="priceItem priceValue">Price</td>
+        <td class="priceItem">Diff</td>
+        <td class='spacer'></td>
+        <td class="quantityItem quantityValue">Stock</td>
+        <td class="quantityItem">Diff</td>
+        <td class="priceItem priceValue">Price</td>
+        <td class="priceItem">Diff</td>
+        <td class='spacer'></td>
+        <td class="quantityItem quantityValue">Stock</td>
+        <td class="quantityItem">Diff</td>
+        <td class="priceItem priceValue">Price</td>
+        <td class="priceItem">Diff</td>
+        <td class='spacer'></td>
+        <td class="quantityItem quantityValue">Stock</td>
+        <td class="quantityItem">Diff</td>
+        <td class="priceItem priceValue">Price</td>
+        <td class="priceItem">Diff</td>
+    </tr>
+  `)
+  table.append(tableHead)
+  data.forEach(row=>{
+    let nameClasses = 'card-name high-density'
+    let namestyling = `background-image: linear-gradient(to right, rgba(0,0,0,0.9) 25%, transparent),url(./data/cards/${row.card_number}.jpg);`
+    let rowString = `
+      <tr class='table-content'>
+      <td class='card-number'>${row.card_number}</td>
+      <td class='${nameClasses}' style="${namestyling}">${row.card_name}</td>
+    `
+
+    sites.forEach((site,index)=> {
+      let quantityDiffValue = row[`${site}_quantity_diff`]
+      let priceDiffValue = row[`${site}_price_diff`]
+      let columnString = `
+        <td class='spacer'></td>
+        <td class='quantityItem quantityValue ${site}'>${row[`${site}_quantity_trend`]}${numberWithCommas(row[`${site}_latest_quantity`])}</td>
+        <td class='quantityItem quantityDiff ${site} ${getColorClass(quantityDiffValue)}'>${formatDiff(quantityDiffValue)}</td>
+        <td class='priceItem priceValue ${site}'><a target="_blank" href="${row[`${site}_url`]}">${row[`${site}_price_trend`]}${numberWithCommas(row[`${site}_latest_price`])}</a></td>
+        <td class='priceItem priceDiff ${site} ${getColorClass(priceDiffValue)}'>${formatDiff(priceDiffValue)}</td>
+      `
+      rowString = rowString + columnString
+    })
+    rowString = rowString + "</tr>"
+    table.append($(rowString))
+  })
+  return table
+}
+
+
+$.ajax({
+    type: "GET",
+    url: "./data/summary.csv",
+    success: function (data) {
+        $('#summary-table').append(parseSummaryTable(Papa.parse(data,{header:true}).data));
+    }
+});
+
+$.ajax({
+  type: "GET",
+  url: './data/main.csv',
+  success: function(data) {
+    let dataArray = Papa.parse(data,{header: true}).data
+    dataArray.pop()
+
+    let urs = dataArray.filter(row=>row.card_rarity == 'UR')
+    $('#ur-table').append(parseMainTable(urs))
+    
+    let csrs = dataArray.filter(row=>row.card_rarity == 'HR')
+    $('#hr-table').append(parseMainTable(csrs))
+
+    let srs = dataArray.filter(row=>row.card_rarity == 'SR')
+    $('#sr-table').append(parseMainTable(srs))
+  }
+});
+
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
